f10a362d7003d94b155e9df32fdc195d63ff8133
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-05 12:49:15 +0100

    Update models.py
    add product fields such as name, amount, grade and images

--- products/models.py
+++ products/models.py
@@ -10,3 +10,16 @@ class Category(models.Model):
 
     def get_easy_name(self):
         return self.easy_name
+    
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL) 
+    sku = models.CharField(max_length=200, null=True, blank=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    grade = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1000, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name   
