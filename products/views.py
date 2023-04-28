b68c5e34f675edc1c577c7988cfb69b7fd305d8b
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-28 14:14:01 +0100

    views.py
    create queries for search using Q function so when we search we will filter out products and also use icontains to make it non case sensitive

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect,reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -6,6 +8,14 @@ from .models import Product
 def all_products(request):
 
     products = Product.objects.all()
+
+    if request.GET:
+        if 'q' in request.GET:
+            inquire = request.GET['q']
+            if not inquire:
+                messages.error(request, "please enter a valid search")
+                return redirect(reverse('products'))
+
 
     context = {
         'products': products,
