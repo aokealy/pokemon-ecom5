c6e48e686ffe329e519f95d2e2f31787c7e59124
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-28 17:11:47 +0100

    index.html
    add a footer template

--- products/views.py
+++ products/views.py
@@ -13,6 +13,9 @@ def all_products(request):
     categories = None
 
     if request.GET:
+        
+    
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
