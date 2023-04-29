d6a5be1a080b39b258915018843bd4766ff593f8
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-29 14:46:13 +0100

    fixed bug context.py
    changed to amount which fixed the bug

--- cart/contexts.py
+++ cart/contexts.py
@@ -9,12 +9,12 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
-    cart = request.session.get('bag', {})
+    cart = request.session.get('cart', {})
 
     for item_id, item_data in cart.items():
-        if isinstance(item_data, int):
+
             product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
+            total += item_data * product.amount
             product_count += item_data
             cart_items.append({
                 'item_id': item_id,
