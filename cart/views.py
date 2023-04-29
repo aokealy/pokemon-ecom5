b9d73ae6cd1fda3e9d6b92b49b3b056718871b5a
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-29 12:44:05 +0100

    product_detail.html
    add a form post method for the user to have add to cart function and have a return to shopping button and add it to views and urls to tell django it exists

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def view_cart(request):
-  return render(request, 'cart/cart.html')
+     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the specified product to the shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('cart', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['cart'] = bag
+    print(request.session['cart'])
+    return redirect(redirect_url)
+
+
+
+
+    
+    
+   
