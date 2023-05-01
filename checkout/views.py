598671bf13733ba86dc833f3c46b8ece3668553a
aokealy <aokealy@gmail.com>
2023-05-01 17:41:20 +0000

    sort stripe keys in views

--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,42 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from cart.contexts import cart_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+
+
     cart = request.session.get('cart', {})
     if not cart:
-        messages.error(request, "There's nothing in your bag at the moment")
+        messages.error(request, "There's nothing in the cart currently")
         return redirect(reverse('products'))
+
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)   
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MzNKpBmLoLbFeg9C5XhIR5FiviBvmdyoqYvifZlkC8uOHZjd4VLyTpnT1hJsIm42lljHpYW30TXopgfLWnpsJoI00FyTPDbR8',
+        'client_secret': 'test client secret',
     }
+    
 
     return render(request, template, context)
