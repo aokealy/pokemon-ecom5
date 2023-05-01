2b85db93d7d68f209943ace7bbf7f9b1c7f494d4
aokealy <104302143+aokealy@users.noreply.github.com>
2023-05-01 09:01:46 +0100

    checkout/models.py
    migrate the models for checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -7,21 +7,46 @@ from django.conf import settings
 from products.models import Product
 
 class Orders(models.Model):
-    order_id = models.CharField(max_length=32, null=False, editable=False)
+    order_id = models.CharField(max_length=40, null=False, editable=False)
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=254, null=False, blank=False)
+    email = models.EmailField(max_length=250, null=False, blank=False)
     phone = models.CharField(max_length=20, null=False, blank=False)
     country = models.CharField(max_length=40, null=False, blank=False)
-    zipcode = models.CharField(max_length=20, null=True, blank=True)
-    city = models.CharField(max_length=40, null=False, blank=False)
+    zipcode = models.CharField(max_length=25, null=True, blank=True)
+    city = models.CharField(max_length=50, null=False, blank=False)
     street_address1 = models.CharField(max_length=90, null=False, blank=False)
     street_address2 = models.CharField(max_length=90, null=True, blank=True)
-    county = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=50, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_id(self):
+        """
+        create a unique ID number for the orders
+        """
+        return uuid.uuid4().hex.upper()
+    
+    def update_total(self):
+       
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+    
+    def save(self, *args, **kwargs):
+       
+        if not self.order_id:
+            self.order_id = self._generate_order_id()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_id
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Orders, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
@@ -29,4 +54,16 @@ class OrderLineItem(models.Model):
     product_size = models.CharField(max_length=2, null=True, blank=True) 
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.amount * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_id}'
+
 
