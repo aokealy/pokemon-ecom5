92cf4963e5a6b6f00f9e501f0948a5c78f1eb1ba
aokealy <104302143+aokealy@users.noreply.github.com>
2023-05-01 09:46:49 +0100

    admin.py
    create fields for admin panel for orders

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Orders, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrdersAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_id', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    fields = ('order_id', 'date', 'first_name', 'last_name', 'email', 'phone', 'country', 'zipcode', 'city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost', 'order_total', 'grand_total',)
+
+    list_display = ('order_id', 'date', 'first_name', 'last_name', 'order_total', 'delivery_cost', 'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Orders, OrdersAdmin)
