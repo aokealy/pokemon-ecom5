e93152a6751d25990bec0cbd498062e7db4b5608
aokealy <104302143+aokealy@users.noreply.github.com>
2023-05-01 14:49:14 +0100

    add stripe public key

--- checkout/views.py
+++ checkout/views.py
@@ -8,6 +8,8 @@ from cart.contexts import cart_contents
 import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your cart currently")
@@ -16,6 +18,13 @@ def checkout(request):
     current_cart = cart_contents(request)
     total = current_cart['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
     
     order_form = OrderForm()
     template = 'checkout/checkout.html'
