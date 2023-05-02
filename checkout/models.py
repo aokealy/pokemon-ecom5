484338a289dfe00ce1d9bd347e3c3ce9a17b724f
aokealy <aokealy@gmail.com>
2023-05-02 00:18:38 +0000

    create profile page

--- checkout/models.py
+++ checkout/models.py
@@ -6,9 +6,12 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
 
 class Orders(models.Model):
     order_id = models.CharField(max_length=40, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=250, null=False, blank=False)
